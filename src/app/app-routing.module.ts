b7b904b50922af304cfea381b0ff251c31839979
Renato Areas <rsareas93@gmail.com>
2022-12-14 21:41:08 -0300

    aprendendo a trabalhar com rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
+import { CriarPensamentosComponent } from './componentes/pensamentos/criar-pensamentos/criar-pensamentos.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {  },
+  { path: '', redirectTo: 'listarPensamento', pathMatch: 'full' },
+
+  { path: 'criarPensamento', component: CriarPensamentosComponent },
+
+  { path: 'listarPensamento', component: ListarPensamentoComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule{};
+export class AppRoutingModule {}
